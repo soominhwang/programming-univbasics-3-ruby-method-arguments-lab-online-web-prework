bd5a4639531779d738815f963680ef9874bf8f1c
Soomin Hwang <soomin.h.hwang@gmail.com>
2019-07-10 04:34:14 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -9,7 +9,6 @@ def introduction_with_language(name, language)
 end
 
 #introduction_with_language_optional
-def introduction_with_language_optional(name, language)
-  language="Ruby"
+def introduction_with_language_optional(name, language="Ruby")
   puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
